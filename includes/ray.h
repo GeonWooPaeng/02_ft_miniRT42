3a17cdc393b4acf62d706d6357cd7c572f905d47
GeonWooPaeng <gwpaeng@naver.com>
2021-01-28 17:31:17 +0900

    move triangle

--- includes/ray.h
+++ includes/ray.h
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.h                                           :+:      :+:    :+:   */
+/*   ray.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/27 14:41:01 by gpaeng            #+#    #+#             */
-/*   Updated: 2021/01/28 15:03:47 by gpaeng           ###   ########.fr       */
+/*   Created: 2021/01/28 14:50:28 by gpaeng            #+#    #+#             */
+/*   Updated: 2021/01/28 14:53:03 by gpaeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECTOR_H
-# define VECTOR_H
+#ifndef RAY_H
+# define RAY_H
 
-#include <math.h>
+#include "vector.h"
 
-typedef struct	s_vec
+typedef t_vec t_point;
+typedef t_vec t_color;
+
+typedef struct	s_ray
 {
-	double x;
-	double y;
-	double z;
-}				t_vec;
+	t_point		orig;	//처음 위치
+	t_vec		dir;	//응시하는 방향
+}				t_ray;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,72 +6,121 @@
 /*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 13:36:51 by gpaeng            #+#    #+#             */
-/*   Updated: 2021/01/27 17:09:15 by gpaeng           ###   ########.fr       */
+/*   Updated: 2021/01/28 17:31:04 by gpaeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "../includes/vector.h"
 
-#define ESC 0xFF1B
+#define UP		126
+#define DOWN	125
+#define LEFT	123
+#define RIGHT	124
+#define ESC		53
+#define KeyPress	2
+#define KeyRelease	3
 
 //키보드 번호를 알수 있게 해주는 것
 
 typedef struct s_data
 {
 	void *mlx;
-	void *win;
+	void *mlx_win;
 	int width, height;
+	int x, y;
+	int up, down, left, right;
 	void *img;
-	char *addr;
-	int bits_per_pixel;
-	int size_line;
+	char *addr; //mlx_get_data_addr()로 생성한 이미지의 시작 주소
+	int bits_per_pixel; //픽셀 당 비트의 수
+	int line_length; //이미지의 가로 한줄의 바이트 수
 	int endian;
-}t_data;
+}				t_data;
 
-int key_press(int key, t_data data)
+void my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	printf("pre_key: %d\n", key);
-	if (key == ESC)
+	char *dst;
+	// y * data->line_length : y좌표만큼의 메모리 주소로 이동
+	// data->bits_per_pixel / 8 : 픽셀당 바이트 수를 구하기 위해 /8 해줍니다.
+	// x * (data->bits_per_pixel / 8) : x 좌표만큼의 메모리 주소를 이동
+	// => (x,y)좌표의 픽셀의 메모리 주소 구할 수 있다.
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int ft_move(t_data *data)
+{//시작이 좌측 상단입니다.
+	if (data->left == 1 && data->x > 0)
+	{
+		data->x -= 3;
+		printf("%x = %d, y = %d\n", data->x, data->y);
+	}
+	if (data->right == 1 && data->x + 100 < data->width)
+	{//삼각형의 너비를 고려해서 100해준 것입니다.
+		data->x += 3;
+		printf("x = %d, y = %d\n", data->x, data->y);
+	}
+	if (data->up == 1 && data->y > 0)
+	{
+		data->y -= 3;
+		printf("x = %d, y = %d\n", data->x, data->y);
+	}
+	if (data->down == 1 && data->y + 100 < data->height)
+	{
+		data->y += 3;
+		printf("x = %d, y = %d\n", data->x, data->y);
+	}
+	return (0);
+}
+
+int ft_key_press(int keycode, t_data *data)
+{
+	if (keycode == ESC)
+	{
+		mlx_destroy_window(data->mlx, data->mlx_win);
 		exit(0);
+	}
+	if (keycode == LEFT)
+		data->left = 1;
+	if (keycode == RIGHT)
+		data->right = 1;
+	if (keycode == UP)
+		data->up = 1;
+	if (keycode == DOWN)
+		data->down = 1;
+	return (0);
 }
 
-int key_release(int key, t_data data)
+int ft_key_release(int keycode, t_data *data)
 {
-	printf("rel_key: %d\n", key);
+	
 }
-
-int mouse_press(int button, int x, int y, void *p)
-{
-	printf("button_num: %d , press at %dx%d\n", button, x, y);
-}
-
-int mouse_release(int button, int x, int y, void *p)
-{
-	printf("button_num: %d, release at %dx%d\n", button, x, y);
-}
-
-int mouse_pos(int x, int y, t_data *data)
-{
-	printf("mouse_pos %dx%d\n", x, y);
-	mlx_pixel_put(data->mlx, data->win, x, y, 0xFFFFFF);
-}
-
 int main(void)
 {
-	t_data data;
+	void *mlx;
+	void *mlx_win;
+	t_data img;
+	int i;
+	int j;
+	int k;
 
-	data.width = 600;
-	data.height = 400;
-	data.mlx = mlx_init();
-	data.win = mlx_new_window(data.mlx, data.width, data.height, "minirt_test");
-	data.img = mlx_new_image(data.mlx, 600, 400);
-	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.size_line, &data.endian);
-	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
-
-	mlx_hook(data.win, 2, 1L<<0, key_press, 0);
-	mlx_hook(data.win, 3, 1L<<1, key_release, 0);
-	mlx_loop(data.mlx);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 600, 400, "miniRT");
+	img.img = mlx_new_image(mlx, 600, 400);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	for (i = 250, k = 99; i < 300; i++, k -= 2)
+	{
+		for (j = 150 + k; j < 250; j++)
+			my_mlx_pixel_put(&img, i, j, 0x0000FF00);
+	}
+	for (i = 300, k = 0; i < 350; i++, k+= 2)
+	{
+		for (j = 150 + k; j < 250; j++)
+			my_mlx_pixel_put(&img, i, j, 0x0000FF00);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 	return (0);
 }
